2c5aa736306c111ef58bde7c833fb59f6a8f77b9
daniil <whitesmurf1312@mail.com>
2020-12-07 14:31:46 +0300

    fix:Fix Sing Up

--- HFB/src/main/java/com/HelpForBlind/HFB/Controllers/SingInController.java
+++ HFB/src/main/java/com/HelpForBlind/HFB/Controllers/SingInController.java
@@ -2,6 +2,7 @@ package com.HelpForBlind.HFB.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class SingInController {
@@ -9,5 +10,10 @@ public class SingInController {
     @GetMapping("/blog/singin")
     public String SingIn(){
         return "SingIn";
+    }
+
+    @PutMapping("/blog/singin")
+    public String PostSingin(){
+        return "redirect:/homepage";
     }
 }
